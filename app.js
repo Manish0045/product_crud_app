072f696f6a7b1262b7edd77818a48a5bf4190e96
Manish Sahani <manishsahani345@gmail.com>
2024-10-02 09:55:46 +0530

    Cors added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.uss(cors());
 
 // Routes
 const productRoutes = require("./Routes/product.route");
